032cf656269a7e531f2644b53279c644c11844c2
Paul MATTHEW <paul.matthew@edenred.com>
2020-04-11 01:46:18 +0800

    #Exercise 2 - Solution

---
+++ Vidly-MVC-5/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_MVC_5.Models;
+
+namespace Vidly_MVC_5.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
